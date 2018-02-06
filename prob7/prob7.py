0b41cb6574fa8f83e30af6cc35a604533130829b
xChaconne <xchaconne@gmail.com>
2018-02-05 23:56:27 -0500

    solved problem 7

---
+++ prob7/prob7.py
@@ -0,0 +1,20 @@
+# taken from Pi Delport's answer at https://stackoverflow.com/questions/3939660
+def primes_sieve2(limit):
+    a = [True] * limit 
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in xrange(i*i, limit, i):
+                a[n] = False
+                
+# Let pi(n) be the prime counting function, i.e.
+# pi(n) = |{p <= n : p is prime}|.
+# the prime number theorem says that pi(n) ~ n / log(n) as n -> infinity
+# we want pi(n) = 10001 so we don't generate any unneccesary primes
+# so we want n : n / log(n) = 10000. This n is around 120000 so we'll use that.
+
+primelist = list(primes_sieve2(120000))
+print primelist[10000]  # 10000 since 0-indexed
+
