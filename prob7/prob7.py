c94cf887f29b8f463f5c44b3714a0af4c39a2d57
xChaconne <xchaconne@gmail.com>
2018-02-06 00:33:44 -0500

    modified comments

--- prob7/prob7.py
+++ prob7/prob7.py
@@ -14,6 +14,8 @@ def primes_sieve2(limit):
 # the prime number theorem says that pi(n) ~ n / log(n) as n -> infinity
 # we want pi(n) = 10001 so we don't generate any unneccesary primes
 # so we want n : n / log(n) = 10000. This n is around 120000 so we'll use that.
+# Also if we're only going up to primes at most 120000 in value I think
+# this sieve should work fine still.
 
 primelist = list(primes_sieve2(120000))
 print primelist[10000]  # 10000 since 0-indexed
