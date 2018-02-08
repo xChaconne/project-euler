759a656b1bcc52a789e67f3e664fd59049a444a8
xChaconne <xchaconne@gmail.com>
2018-02-07 21:24:54 -0500

    reversed direction of divisor search

--- prob3/prob3.py
+++ prob3/prob3.py
@@ -12,6 +12,9 @@
 
 
 from math import sqrt, floor
+from time import time
+import numpy as np
+
 
 def get_largest_factor(n):
     if n % 2 == 0:
@@ -19,20 +22,19 @@ def get_largest_factor(n):
     sqrt_n_flr = floor(sqrt(n))
     maxnum = sqrt_n_flr - 1 if sqrt_n_flr % 2 == 0 else sqrt_n_flr
     
-    for d in range(int(maxnum), -1, -2):
+    # for d in range(int(maxnum), -1, -2):  # probably faster to go in reverse
+    for d in range(3, int(maxnum), 2):
         if n % d == 0:
             return [d, n / d]
         
-# taken from Pi Delport's answer at https://stackoverflow.com/questions/3939660
-def primes_sieve2(limit):
-    a = [True] * limit 
-    a[0] = a[1] = False
 
-    for (i, isprime) in enumerate(a):
-        if isprime:
-            yield i
-            for n in xrange(i*i, limit, i):
-                a[n] = False
+def prime_sieve(maxval):
+    nums = np.ones(maxval).astype(bool)
+    nums[0:2] = False
+    for i, val in enumerate(nums):
+        if val:
+            nums[np.arange(2 * i, maxval, i)] = False
+    return np.where(nums)[0]
 
 
 def get_largest_prime_factor(n):
@@ -43,10 +45,18 @@ def get_largest_prime_factor(n):
             return curr
         facs.extend(get_largest_factor(curr))
         list.sort(facs, reverse=True)
-        
+
+t0 = time()
+
 N = 600851475143
 
-PRIMES = set(primes_sieve2(500000)) # 500000 > max(get_largest_factor(N))
+PRIMES = set(prime_sieve(500000)) # 500000 > max(get_largest_factor(N))
 
-print get_largest_prime_factor(N)
+ans = get_largest_prime_factor(N)
     
+t1 = time()
+
+print 'Answer: {}'.format(ans)
+print 'Finding factors method: {} milliseconds'.format((t1-t0)* 60.)
+
+
